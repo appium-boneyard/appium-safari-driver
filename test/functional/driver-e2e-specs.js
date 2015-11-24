61e1ca955cb2cfb2895a6436cb563ce0d948de62
Jonah Stiennon <jonahss@gmail.com>
2015-11-23 16:53:27 -0800

    working on getting a simple test running. async error logging in place to find the stopping point

--- test/functional/driver-e2e-specs.js
+++ test/functional/driver-e2e-specs.js
@@ -1,43 +1,29 @@
-import { startServer } from '../..';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import { absolute } from 'ios-test-app';
-import wd from 'wd';
+import { SafariDriver } from '../../lib/driver.js';
 
 chai.should();
 chai.use(chaiAsPromised);
 
-const HOST = "localhost",
-      PORT = 4994,
-      APP = absolute.iphonesimulator;
-
 const DEFAULT_CAPS = {
   platformName: 'iOS',
-  platformVersion: '8.4',
-  app: APP,
-  bundleId: "io.appium.TestApp",
-  deviceName: "iPhone 6",
-  udid: "75036442-8970-4D09-9200-E296EEB0C78A",
-  automationName: "WebDriverAgent"
+  platformVersion: '9.1',
+  bundleId: "com.apple.mobilesafari",
+  deviceName: "iPhone 6"
 };
 
 describe('SafariDriver', () => {
-  let server;
-  let driver = wd.promiseChainRemote(HOST, PORT);
-  before(async () => {
-    server = await startServer(PORT, HOST);
+
+  it('gets contexts', async function () {
+    this.timeout(10 * 1000);
+    
+    let driver = new SafariDriver();
+    await driver.createSession(DEFAULT_CAPS);
+
+    let contexts = await driver.getContexts();
+
+    console.log(contexts);
+    contexts.length.should.be.above(0);
   });
 
-  after(() => {
-    // TODO I don't think this is actually shutting the server down, figure
-    // that out
-    server.close();
-  });
-
-  it('should start and stop a session', async () => {
-    await driver.init(DEFAULT_CAPS);
-    let els = await driver.elementsByClassName("UIAButton");
-    els.length.should.equal(7);
-    await driver.quit();
-  });
 });
