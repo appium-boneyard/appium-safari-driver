cc982aa6ac832f5dfdcd6b65d7264ffe9a8b8be3
Jonathan Lipps <jlipps@gmail.com>
2016-05-17 10:36:39 -0700

    use appium-base-driver, fix tests, start sim for tests, and add travis

--- test/functional/driver-e2e-specs.js
+++ test/functional/driver-e2e-specs.js
@@ -1,27 +1,41 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
+import { getDevices } from 'node-simctl';
+import { getSimulator, killAllSimulators } from 'appium-ios-simulator';
 import { SafariDriver } from '../../lib/driver.js';
 
 chai.should();
 chai.use(chaiAsPromised);
 
+const IOS_VER = '9.2';
 const DEFAULT_CAPS = {
   platformName: 'iOS',
-  platformVersion: '9.1',
+  platformVersion: IOS_VER,
   bundleId: "com.apple.mobilesafari",
   deviceName: "iPhone 6"
 };
 
-describe('SafariDriver', () => {
 
-  let driver;
+describe('SafariDriver', function () {
+  this.timeout(100000);
+
+  let driver, sim;
   before(async () => {
+    await killAllSimulators();
+    let devices = await getDevices();
+    if (!devices[IOS_VER] || devices[IOS_VER].length === 0) {
+      throw new Error(`Could not find a ${IOS_VER} sim`);
+    }
+    sim = await getSimulator(devices[IOS_VER][0].udid);
+    await sim.run();
+    await sim.openUrl("http://appium.io");
     driver = new SafariDriver();
     await driver.createSession(DEFAULT_CAPS);
   });
 
   after(async () => {
     await driver.deleteSession();
+    await killAllSimulators();
   });
 
   it('gets contexts', async function () {
@@ -43,7 +57,7 @@ describe('SafariDriver', () => {
 
   it('gets title of first webview', async function () {
 
-    await driver.getUrl('http://www.appium.io');
+    await driver.setUrl('http://www.appium.io');
     let title = await driver.title();
 
     title.should.equal('Appium: Mobile App Automation Made Awesome.');
@@ -51,7 +65,7 @@ describe('SafariDriver', () => {
 
   it('finds elements by class name', async function () {
 
-    await driver.getUrl('http://www.appium.io');
+    await driver.setUrl('http://www.appium.io');
     let heading = await driver.findElOrEls('class name', 'jumbotron', false);
     let text = await driver.getText(heading);
     text.should.contain('Appium');
@@ -60,7 +74,7 @@ describe('SafariDriver', () => {
 
   it('finds elements by css', async function () {
 
-    await driver.getUrl('http://www.appium.io');
+    await driver.setUrl('http://www.appium.io');
     let navBarLinks = await driver.findElOrEls('css', '.navbar-nav-center a', true);
     navBarLinks.length.should.equal(5);
   });
